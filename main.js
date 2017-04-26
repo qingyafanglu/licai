c9fc667693c6fada0553ffb4a541a23a1afca768
qingyafanglu <2547128345@qq.com>
2017-04-26 13:16:39 +0800

    licai

---
+++ main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Steven on 2016/12/7.
+ */
+$(document).ready(function(){
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+           items:1,
+           loop:true,
+           autoplay:true,
+            autoplayTimeout:5000,
+            autoplayHoverPause:true,
+
+        });
+    });
+});
+
+
+
+
